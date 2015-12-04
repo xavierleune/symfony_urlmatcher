f6332d6d8bd01937edd1e01083f93622045515cc
Xavier Leune <xleune@commentcamarche.net>
2015-12-04 11:26:46 +0100

    Update chemins

--- test_priority_1.php
+++ test_priority_1.php
@@ -1,15 +1,15 @@
 <?php
 
-require(__DIR__ . '/../../vendor/autoload.php');
-require(__DIR__ . '/../../src/Symfony/Component/Routing/RequestContext.php');
-//require(__DIR__ . '/../../src/');
-require(__DIR__ . '/../../src/Symfony/Component/Routing/Matcher/RedirectableUrlMatcherInterface.php');
-require(__DIR__ . '/../../src/Symfony/Component/Routing/Matcher/RequestMatcherInterface.php');
-require(__DIR__ . '/../../src/Symfony/Component/Routing/RequestContextAwareInterface.php');
-require(__DIR__ . '/../../src/Symfony/Component/Routing/Matcher/UrlMatcherInterface.php');
-require(__DIR__ . '/../../src/Symfony/Component/Routing/Matcher/UrlMatcher.php');
-require(__DIR__ . '/../../src/Symfony/Component/Routing/Matcher/RedirectableUrlMatcher.php');
-require(__DIR__ . '/../../src/Symfony/Bundle/FrameworkBundle/Routing/RedirectableUrlMatcher.php');
+require(__DIR__ . '/vendor/autoload.php');
+require(__DIR__ . '/vendor/symfony/symfony/src/Symfony/Component/Routing/RequestContext.php');
+//require(__DIR__ . '/vendor/symfony/symfony/src/');
+require(__DIR__ . '/vendor/symfony/symfony/src/Symfony/Component/Routing/Matcher/RedirectableUrlMatcherInterface.php');
+require(__DIR__ . '/vendor/symfony/symfony/src/Symfony/Component/Routing/Matcher/RequestMatcherInterface.php');
+require(__DIR__ . '/vendor/symfony/symfony/src/Symfony/Component/Routing/RequestContextAwareInterface.php');
+require(__DIR__ . '/vendor/symfony/symfony/src/Symfony/Component/Routing/Matcher/UrlMatcherInterface.php');
+require(__DIR__ . '/vendor/symfony/symfony/src/Symfony/Component/Routing/Matcher/UrlMatcher.php');
+require(__DIR__ . '/vendor/symfony/symfony/src/Symfony/Component/Routing/Matcher/RedirectableUrlMatcher.php');
+require(__DIR__ . '/vendor/symfony/symfony/src/Symfony/Bundle/FrameworkBundle/Routing/RedirectableUrlMatcher.php');
 require('priority.php');
 require('original.php');
 
